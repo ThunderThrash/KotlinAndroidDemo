c5fbfc7a7663046779f456aeccb0608eae38f5ee
thunder <gianni.amendola@gmail.com>
2018-03-30 16:22:37 +0200

    added Login Form

--- app/src/main/java/frameschool/net/kotlinandroiddemo/MainActivity.kt
+++ app/src/main/java/frameschool/net/kotlinandroiddemo/MainActivity.kt
@@ -1,23 +1,40 @@
 package frameschool.net.kotlinandroiddemo
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-
-import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        // get reference to all views
+        var et_user_name = findViewById(R.id.et_user_name) as EditText
+        var et_password = findViewById(R.id.et_password) as EditText
+        var btn_reset = findViewById(R.id.btn_reset) as Button
+        var btn_submit = findViewById(R.id.btn_submit) as Button
+
+        btn_reset.setOnClickListener {
+            // clearing user_name and password edit text views on reset button click
+            et_user_name.setText("")
+            et_password.setText("")
+        }
+
+        // set on-click listener
+        btn_submit.setOnClickListener {
+            val user_name = et_user_name.text;
+            val password = et_password.text;
+            Toast.makeText(this@MainActivity, user_name, Toast.LENGTH_LONG).show()
+
+            // your code to validate the user_name and password combination
+            // and verify the same
+
         }
     }
 
