45424185b07ee05ba15310cd74f7829bfb4b7a78
thunder <gianni.amendola@gmail.com>
2018-03-30 16:18:20 +0200

    Initial commit

---
+++ app/src/androidTest/java/frameschool/net/kotlinandroiddemo/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package frameschool.net.kotlinandroiddemo
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("frameschool.net.kotlinandroiddemo", appContext.packageName)
+    }
+}
